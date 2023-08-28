51d4dcbff68b7e024c9345f5b0d80de6eecea4aa
Barış Palabıyık <barispalabiyik@gmail.com>
2023-08-28 20:32:02 +0300

    Updated the title

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// Function to fetch and populate form fields with query params
 function populateFormWithQueryParams() {
-  // Parse the query parameters from the current URL
   const urlParams = new URLSearchParams(window.location.search);
-
-  // List of query params to be fetched
   const queryParams = ["lead_id", "offer_id", "affiliate_id"];
 
-  // Loop through each query parameter and populate the respective form field
   queryParams.forEach((param) => {
     const value = urlParams.get(param);
     if (value !== null) {
-      // Select the div with the custom attribute and then find the input field inside it
       const div = document.querySelector(
         `div[fs-field-validation-name="${param}"]`
       );
@@ -24,7 +18,6 @@ function populateFormWithQueryParams() {
   });
 }
 
-// Run the function when the document is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
   populateFormWithQueryParams();
 });
